59f739f23b9fb29863bdc8e5fa60e1cfe7d3146f
maddylopr <madisonl111@nycstudents.net>
2024-09-25 10:14:03 -0400

    brawl stars

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-x = 3
-y = 1
+# Let's create a function that determines if a number is odd or even
+x = 5
+y = 200
 if x % 2==0 and y % 2==0:
       print("both are even")
 elif x % 2==0 or 2==0:
       print("one is even")
+elif y % 2==0 or 2==0:
+      print("one is even")
 else:
       print("both are odd")
-'''a = 13
-b = 5
 
-c = a % b
-print(a, "mod", b, "=",
-      c, sep="")'''
+# Let's create a function to accept a "bill" value and offer a tip of 0%,
+# 15%, 20% or 25% depending on if the service was "bad, okay, good , 
+# or great ". 
+'''pluh = input("service:")
+if pluh == "bad":
+    print("0%")
+if pluh == "okay":
+    print("15%")
+if pluh == "good":
+    print("20%")
+if pluh == "great":
+    print("25%")'''
+# Create a function that accepts an input and determines all factors of the number. 
